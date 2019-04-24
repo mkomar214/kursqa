f00ab2eb13e36c4d7c04ab6dba469608416543f9
mkomar <mkomar214@gmail.com>
2019-04-24 23:49:08 +0300

    Задачи по массивам

---
+++ src/CLASS1.java
@@ -0,0 +1,67 @@
+import java.lang.reflect.Array;
+
+import java.util.*;
+public class CLASS1 {
+
+
+    public static void class1() {
+
+//        double a = 10;
+//        byte b = (byte) a;
+//        System.out.println(b);
+
+//        double a = 10;
+//        byte b = (byte) a;
+//        System.out.println(b);
+
+
+//        среднеарифметическо массива
+//        int[] arr = {1,2,3,4,5,6,7};
+//        int result=0;
+//        for(int i= 0; i < arr.length; i++ ){
+//            result=result+arr[i];
+//            }
+//        result = result/ arr.length;
+//        System.out.println(result);
+
+//        факториал
+//        int a=5;//
+//        int fact=1;
+//        for (int i=1;i<=a;i++){
+//            fact=fact*i;
+//        System.out.println(fact);
+
+//         массик в встоку
+//        int[] arr={1, 2,3,4,5};
+//        String new_arr = Arrays.toString(arr);
+//        System.out.println(new_arr);
+
+//            строка в массив
+//        String str = "строка";
+//        String[] arrStr = new String[]{str, "абв"};
+//        System.out.println(arrStr[0]);
+//        System.out.println(arrStr[1]);
+
+//        Числовой массив в bool
+//        int[] arr = {1, 2, 3, 4, 5, 0};
+//        Boolean[] arrBool = new Boolean[6];
+//        for (int i = 0; i < arr.length; i++) {
+//            if (arr[i] == 0) {
+//                arrBool[i] = false;
+//            } else {
+//                arrBool[i] = true;
+//            }
+//            System.out.println(arrBool[i]);
+//
+//        }
+
+//        прибавить к кажому элементу hello
+//        String[] arr = {"a","b","c"};
+//        for (int i = 0 ;i<arr.length; i++ ){
+//            arr[i]= arr[i]+" hello";
+//            System.out.println(arr[i]);
+
+//        }
+        
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,38 @@
-import java.util.Scanner;
+import java.lang.reflect.Array;
+import java.util.*;
 
 public class Main {
 
 
-    public static void main (String... arg){
+    public static void main (String... arg) {
+//          конефеты
+//        Scanner in = new Scanner(System.in);  // Задача с фантиками
+//        System.out.println("Ведите колличество конфет : ");
+//        int konf = in.nextInt();
+//        System.out.println("Ведите курс обмена :  ");
+//        int kurs = in.nextInt();
+//        int fant = 0;// фантики
+//          if (kurs == 1){
+//              System.out.println("Объелись");
+//              return;
+//              }
+//        for (int i = konf; i>=kurs;){
+//            fant=i%kurs;
+//            konf=konf+i/kurs;
+//             i=i/kurs+fant;
+//            if (i<kurs){
+//                System.out.println(konf + " сожрали");
+//                System.out.println(i + " остаток фаньтиков");
+//
 
-        Scanner in = new Scanner(System.in);
-        System.out.println("Ведите колличество конфет : ");
-        int konf = in.nextInt();
-        System.out.println("Ведите курс обмена :  ");
-        int kurs = in.nextInt();
-        int fant = 0;// фантики
+//            }
 
-        for (int i = konf; i>=kurs;){
-            fant=i%kurs;
-            konf=konf+i/kurs;
-             i=i/kurs+fant;
-            if (i<kurs){
-                System.out.println(konf + " сожрали");
-                System.out.println(i + " остаток фаньтиков");
-            }
+//
+//        }
 
-        }
 
+        CLASS1.class1();
     }
-}
+   }
+
 
