dd2a1300003d3a62047db5e0ba9c75eebba6ecdd
mkomar <mkomar214@gmail.com>
2019-06-03 10:34:24 +0300

    Задачи по ООП и модификаторам

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
 
     public static void main(String... arg) {
-        System.out.println(Validate.validMethod("{asda}}{"));
+        System.out.println(Validate.validMethod(""));
 
 
 
--- src/Validate.java
+++ src/Validate.java
@@ -1,34 +1,30 @@
-import java.util.HashMap;
 import java.util.Stack;
 
 public class Validate {
 
-    public static boolean validMethod(String s){
+    public static boolean validMethod(String s) {
 
-        HashMap<Character, Character> map = new HashMap<>();
-        map.put('}', '{');
+        {
+            if (s.isEmpty())
+                return true;
 
-        Stack<Character> stack = new Stack<Character>();
-
-        for (int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
-
-            if (map.containsKey(c)) {
-
-                            char topChar;
-                            if( stack.empty()) {
-                                topChar = 'q';
-                            } else topChar = stack.pop();
-
-                        if (topChar != map.get(c)) {
-                            return false;
-                            }
-            } else {
-
-                stack.push(c);
+            Stack<Character> stack = new Stack<>();
+            for (int i = 0; i < s.length(); i++) {
+                char c = s.charAt(i);
+                if (c == '{' || c == '(' || c == '[') {
+                    stack.push(c);
+                }
+                if (c == '}' || c == ')' || c == ']') {
+                    if (stack.isEmpty())
+                        return false;
+                    char last = stack.peek();
+                    if (c == '}' && last == '{' || c == ')' && last == '(' || c == ']' && last == '[')
+                        stack.pop();
+                    else
+                        return false;
+                }
             }
+            return stack.isEmpty();
         }
-
-        return stack.isEmpty();
     }
 }
